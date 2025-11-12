d431a2a5bbcf0fbd29464ef2197ead715fc3b2c8
Gerard Aguilar <gerard.aguilar1@insbaixcamp.cat>
2025-11-12 15:39:06 +0100

    modificacio contar vocals

--- AEA1/RepasExamen/contarvocals.js
+++ AEA1/RepasExamen/contarvocals.js
@@ -1,4 +1,4 @@
-
+//Contar la vocal "a" en una paraula escrita per l'usuari
 // let paraula = prompt("Escriu una paraula per a contar les 'a':");
 
 // let numAs = paraula.split("").filter(lletra => lletra === "a").length;
@@ -6,7 +6,7 @@
 // console.log(numAs);        // ["a", "a", "a", "a"]
 // console.log(`La paraula '${paraula}' té ${numAs} vocals 'a'.`);
 
-/*
+/* Comptar totes les vocals d'una paraula donada
 let paraula = "allargada";
 let comptador = 0;
 let vocals = "aeiou";
@@ -20,7 +20,7 @@ for (let lletra of paraula) {
 console.log(`La paraula '${paraula}' té ${comptador} vocals en total.`);
 */
 
-
+//contar la vocal "a" en una frase escrita per l'usuari
 let frase = prompt("Escriu una frase per a contar les 'a':");
 
 let num = frase.split("").filter(lletra => lletra.toLowerCase() === "a").length;
