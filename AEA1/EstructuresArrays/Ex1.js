b0336ad56331247fab0ab5a3b944a91d3d516718
Gerard Aguilar <gerard.aguilar1@insbaixcamp.cat>
2026-02-18 12:48:09 +0100

    exercici estructures acabat

--- AEA1/EstructuresArrays/Ex1.js
+++ AEA1/EstructuresArrays/Ex1.js
@@ -9,13 +9,29 @@ let productes = [
 productes.push(["motxilla", 39.99, 12]);
 
 //Esborrem un producte
-productes.splice(3,1);
+productes.splice(3, 1);
 
 //Filtrar producte amb descompte
-productes.map() = [
-  ["camisa", 19.99,20],
-  ["pantalons", 29.99, 15],
-  ["sabates", 49.99, 10],
-  ["motxilla", 39.99,12]
-];
+let productesAmbDescompte = productes.map(producte => {
+  let [nom, preu, quantitat] = producte;
+  if (preu > 30) {
+    preu *= 0.9; // 10% de descompte
+  }
+  return [nom, preu, quantitat];
+});
+
+console.log("Productes amb descompte:", productesAmbDescompte);
+
+//Filtrar producte amb més de 10 unitats en estoc
+let productesEnEstoc = productes.filter(producte => producte[2] > 10);
+
+console.log("Productes amb més de 10 unitats:", productesEnEstoc);
+
+//Calcular el valor total de l'inventari
+let valorTotal = productes.reduce((total, producte) => {
+  let [nom, preu, quantitat] = producte;
+  return total + (preu * quantitat);
+}, 0);
+
+console.log("Valor total de l'inventari:", valorTotal.toFixed(2));
 
